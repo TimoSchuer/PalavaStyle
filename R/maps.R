248d947f6efcec25876cc43f6d996717f0e8e6c5
TimoSchuer <timo.schuermann91@gmx.de>
2025-08-19 16:27:02 +0200

    add data argument to basemap

--- R/maps.R
+++ R/maps.R
@@ -3,6 +3,9 @@
 #' Erstellt eine Grundkarte von Nordrhein-Westfalen mit dem Umriss aus nrwShape.
 #' Die Karte kann durch weitere ggplot2 Layer erweitert werden.
 #'
+#' @param data Optional: sf-Objekt mit Daten die auf der Karte dargestellt werden sollen.
+#'   Wenn angegeben, wird das data-Argument an ggplot() weitergegeben und kann in
+#'   nachfolgenden Layern verwendet werden.
 #' @param bg Hintergrundfarbe der Karte (Standard: "lightgrey")
 #' @param outline_color Farbe des NRW-Umrisses (Standard: "black")
 #' @param outline_size Strichstärke des Umrisses (Standard: 0.5)
@@ -20,24 +23,23 @@
 #' # Grundkarte mit angepassten Farben
 #' grundkarte_nrw(bg = "white", outline_color = "#222755")
 #'
-#' # Grundkarte erweitert mit Gemeinden
+#' # Grundkarte mit Daten - traditionelle Methode
 #' grundkarte_nrw() +
 #'   geom_sf(data = nrwGemPolygons,
 #'           fill = "transparent",
 #'           color = "grey50",
 #'           size = 0.2)
 #'
-#' # Grundkarte mit Daten und PALAVA-Styling
-#' grundkarte_nrw(bg = "#f0efec") +
-#'   geom_sf(data = nrwGemPoints,
-#'           aes(color = AGS),
-#'           size = 1) +
+#' # Grundkarte mit Daten - data Parameter
+#' grundkarte_nrw(data = nrwGemPoints) +
+#'   geom_sf(aes(color = AGS), size = 1) +
 #'   scale_color_palava_discrete() +
 #'   theme_palava_map() +
 #'   labs(title = "NRW Gemeindezentren") +
 #'   labs_palava() +
 #'   theme(legend.position = "none")
 grundkarte_nrw <- function(
+  data = NULL,
   bg = "lightgrey",
   outline_color = "black",
   outline_size = 0.5
@@ -49,7 +51,7 @@ grundkarte_nrw <- function(
     )
   }
 
-  ggplot2::ggplot() +
+  ggplot2::ggplot(data = data) +
     # Hintergrund (Bounding Box von NRW)
     ggplot2::geom_sf(
       data = nrwShape,
@@ -68,6 +70,9 @@ grundkarte_nrw <- function(
 #' Erstellt eine Grundkarte von Nordrhein-Westfalen mit Voronoi-Kacheln aus nrwVoronoi.
 #' Die Karte kann durch weitere ggplot2 Layer erweitert werden.
 #'
+#' @param data Optional: sf-Objekt mit Daten die auf der Karte dargestellt werden sollen.
+#'   Wenn angegeben, wird das data-Argument an ggplot() weitergegeben und kann in
+#'   nachfolgenden Layern verwendet werden.
 #' @param voronoi_fill Füllfarbe der Voronoi-Kacheln (Standard: "white")
 #' @param voronoi_color Randfarbe der Voronoi-Kacheln (Standard: "grey80")
 #' @param voronoi_size Strichstärke der Voronoi-Ränder (Standard: 0.2)
@@ -94,16 +99,23 @@ grundkarte_nrw <- function(
 #' # Voronoi-Karte ohne NRW-Umriss
 #' grundkarte_nrw_voronoi(show_outline = FALSE)
 #'
-#' # Voronoi-Karte mit Daten und PALAVA-Styling
-#' grundkarte_nrw_voronoi(voronoi_fill = "white") +
-#'   geom_sf(data = nrwGemPoints,
-#'           aes(color = factor(substr(AGS, 1, 2))),
-#'           size = 1.5) +
+#' # Voronoi-Karte mit Daten - data Parameter
+#' grundkarte_nrw_voronoi(data = nrwGemPoints, voronoi_fill = "white") +
+#'   geom_sf(aes(color = factor(substr(AGS, 1, 2))), size = 1.5) +
 #'   scale_color_palava_discrete(name = "Regierungsbezirk") +
 #'   theme_palava_map() +
 #'   labs(title = "NRW Gemeinden in Voronoi-Kacheln") +
 #'   labs_palava()
+#'
+#' # Voronoi-Kacheln eingefärbt nach eigenen Daten
+#' \dontrun{
+#' grundkarte_nrw_voronoi(data = my_voronoi_data) +
+#'   geom_sf(aes(fill = value), color = "white", size = 0.1) +
+#'   scale_fill_palava_continuous() +
+#'   theme_palava_map()
+#' }
 grundkarte_nrw_voronoi <- function(
+  data = NULL,
   voronoi_fill = "white",
   voronoi_color = "grey80",
   voronoi_size = 0.2,
@@ -126,7 +138,7 @@ grundkarte_nrw_voronoi <- function(
   }
 
   # Basis-Plot mit Voronoi-Kacheln
-  p <- ggplot2::ggplot() +
+  p <- ggplot2::ggplot(data = data) +
     ggplot2::geom_sf(
       data = nrwVoronoi,
       fill = voronoi_fill,
